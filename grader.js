92c97da7c72cf3e1dd403a64d1dd4bd2c50056c8
Kap <mike@mikekaplan.net>
2013-07-14 04:18:58 +0000

    Updated versions of checks.json and grader.js.

--- grader.js
+++ grader.js
@@ -20,9 +20,12 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
+var sys = require('util');
 var HTMLFILE_DEFAULT = "index.html";
+var URL_DEFAULT = "http://bitstarterkap.herokuapp.com";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
@@ -30,7 +33,9 @@ var assertFileExists = function(infile) {
   if (!fs.existsSync(instr)) {
     console.log("%s does not exist. Exiting.", instr);
     process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-  };
+  }
+  return instr;
+};
 
 var cheerioHtmlFile = function(htmlfile) {
   return cheerio.load(fs.readFileSync(htmlfile));
@@ -45,7 +50,7 @@ var checkHtmlFile = function(htmlfile, checksfile) {
   var checks = loadChecks(checksfile).sort();
   var out = {};
   for (var ii in checks) {
-    var present = $(check[ii]).length > 0;
+    var present = $(checks[ii]).length > 0;
     out[checks[ii]] = present;
   }
   return out;
@@ -61,6 +66,7 @@ if (require.main == module) {
   program
     .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
     .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-u, --url <url>', 'URL', clone(assertFileExists), URL_DEFAULT)
     .parse(process.argv);
   var checkJson = checkHtmlFile(program.file, program.checks);
   var outJson = JSON.stringify(checkJson, null, 4);
